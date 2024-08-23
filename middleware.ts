e167c7a5e1c00a6d375cf93d465599b7f1744f15
Numan Ali <numan4599322ali@gmail.com>
2024-08-23 15:33:41 +0530

    first

--- middleware.ts
+++ middleware.ts
@@ -30,18 +30,16 @@
 //     '/(api|trpc)(.*)',
 //   ],
 // }
+import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
 
-import { authMiddleware } from '@clerk/nextjs/server'
+const isProtectedRoute = createRouteMatcher(["/profile"]);
 
-export default authMiddleware({
-  ignoredRoutes: ['/api/webhooks(.*)'],
-})
+export default clerkMiddleware((auth, req) => {
+  if (isProtectedRoute(req)) {
+    auth().protect();
+  }
+});
 
 export const config = {
-  matcher: [
-    // Skip Next.js internals and all static files, unless found in search params
-    '/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)',
-    // Always run for API routes
-    '/(api|trpc)(.*)',
-  ],
-}
+  matcher: ["/((?!.*\\..*|_next).*)", "/", "/(api|trpc)(.*)"],
+};
